561e5dc87c810cd591441d4d4fb6ed59b48670ea
marcsj <marc@marcsj.com>
2020-02-19 19:59:59 -0800

    Created skeleton code for notes server.

---
+++ notes_server.go
@@ -0,0 +1,56 @@
+package jotfly
+
+import (
+	"context"
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/marcsj/jotfly-server/notes"
+)
+
+type notesServer struct {
+}
+
+func NewNotesServer() notes.NotesServer {
+	return &notesServer{}
+}
+
+func (s notesServer) GetDirectories(
+	ctx context.Context,
+	req *notes.GetDirectoriesRequest,
+	) (*notes.GetDirectoriesResponse, error) {
+	return nil, nil
+}
+
+func (s notesServer) GetDirectoryNotes(
+	ctx context.Context,
+	req *notes.GetDirectoryNotesRequest,
+	) (*notes.GetDirectoryNotesResponse, error) {
+	return nil, nil
+}
+
+func (s notesServer) GetNote(
+	ctx context.Context,
+	req *notes.GetNoteRequest,
+	) (*notes.Note, error) {
+	return nil, nil
+}
+
+func (s notesServer) CreateNote(
+	ctx context.Context,
+	req *notes.CreateRequest,
+	) (*empty.Empty, error) {
+	return nil, nil
+}
+
+func (s notesServer) UpdateNote(
+	ctx context.Context,
+	req *notes.UpdateRequest,
+	) (*notes.Note, error) {
+	return nil, nil
+}
+
+func (s notesServer) DeleteNote(
+	ctx context.Context,
+	req *notes.DeleteRequest,
+	) (*empty.Empty, error) {
+	return nil, nil
+}
