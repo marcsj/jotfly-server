05866e98e67e8b5117d098781f52e5b5656208cf
marcsj <marc@marcsj.com>
2020-03-01 12:16:05 -0800

    Removed todo list, accidentally added to code.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,12 +6,6 @@ import (
 )
 
 func main() {
-	//TODO: create notes controller, validation checking, server integration
-	//TODO: more testing
-	//TODO: make flags setup an admin account, with password
-	//TODO: make integration tests and do everything important
-	//TODO: add things to the readme on how to run, what the purpose of jotfly is
-
 	server.NewNotesServer()
 	server.NewUsersServer()
 	log.Println("started joyfly server")
