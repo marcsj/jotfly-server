9fed821f5576c4f7bb074e62792d885930a3fc64
marcsj <marc@marcsj.com>
2020-02-29 22:00:35 -0800

    Added user controller functions for creating and using directories.

--- users/controller.go
+++ users/controller.go
@@ -19,13 +19,46 @@ func (c controller) Login(userID string, password []byte) (*Session, error) {
 }
 
 func (c controller) GetDirectories(userID string) ([]string, error) {
-	return nil, nil
+	user, err := c.repo.GetUser(userID)
+	if err != nil {
+		return nil, err
+	}
+	return user.GetDirectories(), nil
 }
 
-func (c controller) CreateUser(userID string, password []byte, role UserInfo_Role) error {
-	return nil
+func (c controller) addDirectory(userID string, directory string) error {
+	user, err := c.repo.GetUser(userID)
+	if err != nil {
+		return err
+	}
+	user.Directories = append(user.Directories, directory)
+	_, err = c.repo.UpdateUser(userID, user)
+	return err
 }
 
-func (c controller) checkPassword(userID string, password []byte) error {
-	return nil
+func (c controller) CreateUser(userID string, password string, role UserInfo_Role) error {
+	salt, err := generateRandomBytes(16)
+	if err != nil {
+		return err
+	}
+	hashedPass := generatePassword(password, salt)
+	err = c.repo.CreateUser(userID, hashedPass)
+	if err != nil {
+		return err
+	}
+	_, err = c.repo.UpdateUser(userID, &UserInfo{
+		Password: hashedPass,
+		Salt: salt,
+		Role: role,
+		Directories: make([]string, 0),
+	})
+	return err
+}
+
+func (c controller) checkPassword(userID string, password string) error {
+	user, err := c.repo.GetUser(userID)
+	if err != nil {
+		return err
+	}
+	return checkPassword(password, user.GetPassword(), user.GetSalt())
 }
