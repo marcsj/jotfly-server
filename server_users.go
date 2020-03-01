13875c9e5e0564a6c1d4b7452ccf947b00b1aea3
marcsj <marc@marcsj.com>
2020-03-01 08:53:52 -0800

    Added controller functions to users server, and changed proto to correctly send back multiple directories.

--- server_users.go
+++ server_users.go
@@ -2,23 +2,52 @@ package jotfly
 
 import (
 	"context"
+	"errors"
 	"github.com/marcsj/jotfly-server/users"
+	"google.golang.org/grpc/metadata"
 )
 
 type usersServer struct {
+	controller users.Controller
 }
 
-func NewUsersServer() users.UsersServer {
-	return &usersServer{}
+func NewUsersServer(controller users.Controller) users.UsersServer {
+	return &usersServer{
+		controller: controller,
+	}
 }
 
 func (s usersServer) GetDirectories(
 	ctx context.Context,
 	in *users.GetDirectoriesRequest) (*users.GetDirectoriesResponse, error) {
-	return nil, nil
+	id, err := getId(ctx)
+	if err != nil {
+		return nil, err
+	}
+	directories, err := s.controller.GetDirectories(id)
+	if err != nil {
+		return nil, err
+	}
+	return &users.GetDirectoriesResponse{Directories: directories}, nil
 }
 
 func (s usersServer) Login(
 	ctx context.Context, in *users.User) (*users.Session, error) {
-	return nil, nil
+	token, err := s.controller.Login(in.GetId(), in.GetPassword())
+	if err != nil {
+		return nil, err
+	}
+	return &users.Session{Token: token}, nil
+}
+
+func getId(ctx context.Context) (string, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return "", errors.New("no id in context")
+	}
+	id := md.Get("id")
+	if len(id) < 1 {
+		return "", errors.New("no id in context")
+	}
+	return id[0], nil
 }
